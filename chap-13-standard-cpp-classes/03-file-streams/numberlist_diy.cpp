5e43591febb4f7f4343331ae199ec4b44da015c3
Tri Phan <phanphutri@gmail.com>
2024-01-22 06:57:28 +0700

    Updated

--- chap-13-standard-cpp-classes/03-file-streams/numberlist_diy.cpp
+++ chap-13-standard-cpp-classes/03-file-streams/numberlist_diy.cpp
@@ -1,105 +1,45 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <limits>
 #include <fstream>
 
 using std::cout;
 using std::cin;
 using std::vector;
 using std::string;
-using std::ofstream;
-using std::ifstream;
-
-int get_int(string prompt = "")
-{
-    int value;
-    cout << prompt;
-    while (!(cin >> value))
-    {
-        // Report error and re-prompt
-        cout << "Bad entry! The value you entered is not an integer.\n"
-             << "Please try again: ";
-        // Clean up the input stream
-        cin.clear(); // Clear the error state of the stream
-        // Empty the keyboard buffer
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-    }
-    return value;
-}
 
 void print_vector(const vector<int>& v)
 {
-    int len = v.size();
     cout << "{";
-    if (len > 0)
+    int len = v.size();
+    for (int i = 0; i < len; i++)
     {
-        for (int i = 0; i < len; i++)
+        if (i == len - 1)
         {
-            if (i == len - 1)
-            {
-                cout << v[i];
-            }
-            else{
-                cout << v[i] << ", ";
-            }
+            cout << v[i];
+        }
+        else
+        {
+            cout << v[i] << ", ";
         }
     }
     cout << "}\n";
-}
-
-void load_vector_2(const string& filename, vector<int>& v)
-{
-    // Opent file for reading
-    ifstream in(filename);
-    int value;
-    while (in >> value)
-    {
-        v.push_back(value);
-    }
-}
-
-/**
- * load_vector(filename, v)
- *  Reads the contents of vector v from text file filename
- *  v's contents are replaced by the file's contents.
- *  If the file cannot be found, the vector v is empty
- *  v is a vector holding integers.
-*/
-void load_vector(const string& filename, vector<int>& v)
-{
-    // Open a text file for reading
-    ifstream in(filename);
-    if (in.good()) // Make sure the file was opened properly
-    {
-        v.clear(); // Start with empty vector
-        int value;
-        while (in >> value) // Read until end of file
-        {
-            v.push_back(value);
-        }
-    }
-    else
-    {
-        cout << "Unable to load in the file\n";
-    }
-    
 }
 
 void menu()
 {
     cout << "I)nsert <item> "
          << "P)rint "
-         << "S)ave <filename> "
+         << "S)ave <filename> " 
          << "L)oad <filename> "
          << "E)rase "
          << "Q)uit: ";
 }
 
-void run(vector<int>& v, bool done)
+void run(vector<int>& v)
 {
+    bool done = false;
     int value;
-    string filename;
     char command;
     while (!done)
     {
@@ -107,70 +47,63 @@ void run(vector<int>& v, bool done)
         cin >> command;
         switch (command)
         {
-        case 'I':
-        case 'i':
-            // cin >> value;
-            value = get_int();
-            v.push_back(value);
-            break;
-        case 'P':
-        case 'p':
-            print_vector(v);
-            break;
-        case 'S':
-        case 's':
-            cout << "command: " << command << '\n';
-            break;
-        case 'L':
-        case 'l':
-            cin >> filename;
-            load_vector_2(filename, v);
-            break;
-        case 'E':
-        case 'e':
-            v.clear();
-            cout << "All contents in the vector were cleared.\n";
-            break;
-        case 'Q':
-        case 'q':
-            cout << "command: " << command << '\n';
-            done = true;
-            break;
-        
-        default:
-            cout << "Wrong choice. Please try again!\n";
-            break;
-        }
+            case 'I':
+            case 'i':
+                cin >> value;
+                v.push_back(value);
+                break;
+            case 'P':
+            case 'p':
+                print_vector(v);
+                break;
+            case 'S':
+            case 's':
+                cout << "command: " << command << '\n';
+                break;
+            case 'L':
+            case 'l':
+                cout << "command: " << command << '\n';
+                break;
+            case 'E':
+            case 'e':
+                v.clear();
+                cout << "Contents in the vector were cleared!\n";
+                break;
+            case 'Q':
+            case 'q':
+                done = true;
+                cout << "Goodbye!\n";
+                break;
+            
+            default:
+                cout << "Wrong choice. Please try again!\n";
+                break;
+        }    
     }
 }
 
 int main()
 {
     vector<int> list;
-    bool done = false;
-    run(list, done);
+    run(list);
 }
 
-// int main()
+// void run(vector<int>& v, bool& done)
 // {
-//     vector<int> list;
-//     bool done = false;
 //     int value;
 //     char command;
-//     while (!done)
+//     menu();
+//     cin >> command;
+//     switch (command)
 //     {
-//         menu();
-//         cin >> command;
-//         switch (command)
-//         {
 //         case 'I':
 //         case 'i':
 //             cin >> value;
-//             list.push_back(value);
+//             v.push_back(value);
 //             break;
 //         case 'P':
 //         case 'p':
-//             print_vector(list);
+//             print_vector(v);
 //             break;
 //         case 'S':
 //         case 's':
@@ -182,18 +115,19 @@ int main()
 //             break;
 //         case 'E':
 //         case 'e':
-//             list.clear();
-//             cout << "All contents in the vector were cleared.\n";
+//             cout << "command: " << command << '\n';
 //             break;
 //         case 'Q':
 //         case 'q':
-//             cout << "command: " << command << '\n';
 //             done = true;
+//             cout << "Goodbye!\n";
 //             break;
         
 //         default:
 //             cout << "Wrong choice. Please try again!\n";
 //             break;
-//         }
 //     }
 // }
+
+
+
