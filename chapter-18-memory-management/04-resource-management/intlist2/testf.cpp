5fb4c4e3978a2cb85dd4ed622a6feb66507115fc
Tri Phan <phanphutri@gmail.com>
2024-02-27 12:30:21 +0700

    Refactor memory management in IntList classes

--- chapter-18-memory-management/04-resource-management/intlist2/testf.cpp
+++ chapter-18-memory-management/04-resource-management/intlist2/testf.cpp
@@ -1,25 +1,17 @@
 #include <iostream>
-#include "intlist3.h"
+#include "intlist2.h"
 
 using std::cout;
 
 void f()
 {
-    IntList3 my_list; // Contructor called here
+    IntList3 my_list;
     my_list.insert(22);
     my_list.insert(5);
     my_list.insert(-44);
     my_list.print();
     // my_list goes out of scope; destructor automatically frees its memory
 }
-// ->
-// Creating node 22 (1880822993552)
-// Creating node 5 (1880822994384)
-// Creating node -44 (1880822993584)
-// 22 5 -44
-// Destroying node 22 (1880822993552)
-// Destroying node 5 (1880822994384)
-// Destroying node -44 (1880822993584)
 
 // Use pointer
 void f2()
